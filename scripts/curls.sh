538619dc599fa260ee3127f32b0f1794eb803428
Steve White <stevemichaelwhite@gmail.com>
2025-12-30 15:28:00 +1300

    Added sqlx. Modularized files.

---
+++ scripts/curls.sh
@@ -0,0 +1,1 @@
+curl -v http://127.0.0.1:8000/health_check
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hi, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Could not bind to port 8000");
+    run(listener)?.await
 }
+
+
